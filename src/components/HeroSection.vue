0ca234993365445e6bd37bb2ab478b15459487f4
suartanagit <suartanadp@gmail.com>
2026-05-23 11:07:17 +0800

    fix: update HeroSection styling and clean up code

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,10 +2,7 @@
 
 </script>
 <template>
-  <section
-    class="min-h-screen bg-cover bg-center flex "
-    style="background-image: url('/src/assets/images/hero.jpg')"
-  >
+  <section class="min-h-screen bg-cover bg-center flex bg-url[('/src/assets/images/hero.jpg')]">
     <!-- OVERLAY CONTENT -->
     <div class="flex-1 bg-black/25 flex items-center justify-center text-center px-6">
       <div class="text-white">
@@ -16,14 +13,11 @@
         <p class="mt-3 text-lg md:text-xl opacity-90">Saatnya berpakaian sesuai impian.</p>
 
         <div class="flex flex-row justify-center items-center gap-3">
-          <button
-            class="mt-6 bg-white text-black px-7 py-3 rounded-full font-semibold hover:scale-105 transition"
-          >
+          <button class="mt-6 bg-white text-black px-7 py-3 rounded-full font-semibold hover:scale-105 transition">
             Belanja
           </button>
           <button
-            class="mt-6 bg-black/20 text-white border border-white px-7 py-3 rounded-full font-semibold hover:scale-105 transition"
-          >
+            class="mt-6 bg-black/20 text-white border border-white px-7 py-3 rounded-full font-semibold hover:scale-105 transition">
             Jelajahi
           </button>
         </div>
